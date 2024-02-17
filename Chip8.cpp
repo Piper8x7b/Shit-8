9bc6bb09d4b1f1e3fadae09c7c5ed411f7de3acc
Piper <Piper8x7b@gmail.com>
2024-02-17 16:03:21 -0700

    Setup the function pointer table

--- Chip8.cpp
+++ Chip8.cpp
@@ -45,6 +45,76 @@ Chip8::Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().cou
 
     // Init rng
     randByte = std::uniform_int_distribution<uint8_t>(0, 255u);
+
+    // Init the function pointer table
+    table[0x0] = &Chip8::Table0;
+    table[0x1] = &Chip8::OP_1nnn;
+    table[0x2] = &Chip8::OP_2nnn;
+    table[0x3] = &Chip8::OP_3xkk;
+    table[0x4] = &Chip8::OP_4xkk;
+    table[0x5] = &Chip8::OP_5xy0;
+    table[0x6] = &Chip8::OP_6xkk;
+    table[0x7] = &Chip8::OP_7xkk;
+    table[0x8] = &Chip8::Table8;
+    table[0x9] = &Chip8::OP_9xy0;
+    table[0xA] = &Chip8::OP_Annn;
+    table[0xB] = &Chip8::OP_Bnnn;
+    table[0xC] = &Chip8::OP_Cxkk;
+    table[0xD] = &Chip8::OP_Dxyn;
+    table[0xE] = &Chip8::TableE;
+    table[0xF] = &Chip8::TableF;
+
+    for (size_t i = 0; i <= 0xE; i++) {
+        table0[i] = &Chip8::OP_NULL;
+        table8[i] = &Chip8::OP_NULL;
+        tableE[i] = &Chip8::OP_NULL;
+    }
+
+    table0[0x0] = &Chip8::OP_00E0;
+    table0[0xE] = &Chip8::OP_00EE;
+
+    table8[0x0] = &Chip8::OP_8xy0;
+    table8[0x1] = &Chip8::OP_8xy1;
+    table8[0x2] = &Chip8::OP_8xy2;
+    table8[0x3] = &Chip8::OP_8xy3;
+    table8[0x4] = &Chip8::OP_8xy4;
+    table8[0x5] = &Chip8::OP_8xy5;
+    table8[0x6] = &Chip8::OP_8xy6;
+    table8[0x7] = &Chip8::OP_8xy7;
+    table8[0xE] = &Chip8::OP_8xyE;
+
+    tableE[0x1] = &Chip8::OP_ExA1;
+    tableE[0xE] = &Chip8::OP_Ex9E;
+
+    for (size_t i = 0; i <= 0x65; i++) {
+        tableF[i] = &Chip8::OP_NULL;
+    }
+
+    tableF[0x07] = &Chip8::OP_Fx07;
+    tableF[0x0A] = &Chip8::OP_Fx0A;
+    tableF[0x15] = &Chip8::OP_Fx15;
+    tableF[0x18] = &Chip8::OP_Fx18;
+    tableF[0x1E] = &Chip8::OP_Fx1E;
+    tableF[0x29] = &Chip8::OP_Fx29;
+    tableF[0x33] = &Chip8::OP_Fx33;
+    tableF[0x55] = &Chip8::OP_Fx55;
+    tableF[0x65] = &Chip8::OP_Fx65;
+}
+
+void Chip8::Table0() {
+    ((*this).*(table0[opcode & 0x000Fu]))();
+}
+
+void Chip8::Table8() {
+    ((*this).*(table8[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableE() {
+    ((*this).*(tableE[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableF() {
+    ((*this).*(tableF[opcode & 0x00FFu]))();
 }
 
 void Chip8::LoadRom(char const *filename) {
@@ -69,6 +139,11 @@ void Chip8::LoadRom(char const *filename) {
         // Free the buffer
         delete[] buffer;
     }
+}
+
+// Do nothing
+void Chip8::OP_NULL() {
+
 }
 
 // CLS (clear screen)
@@ -368,7 +443,7 @@ void Chip8::OP_ExA1() {
 }
 
 // LD Vx, DT (Set Vx to be the delay timer value)
-void Chip8::OP_Fx06() {
+void Chip8::OP_Fx07() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
 
     registers[Vx] = delayTimer;
@@ -483,7 +558,7 @@ void Chip8::OP_Fx33() {
 void Chip8::OP_Fx55() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
 
-    for (uint8_t i = 0; i <= Vx; i++){
+    for (uint8_t i = 0; i <= Vx; i++) {
         memory[index + i] = registers[i];
     }
 }
@@ -492,8 +567,7 @@ void Chip8::OP_Fx55() {
 void Chip8::OP_Fx65() {
     uint8_t Vx = (opcode & 0x0F00u) >> 8u;
 
-    for (uint8_t i = 0; i <= Vx; ++i)
-    {
+    for (uint8_t i = 0; i <= Vx; ++i) {
         registers[i] = memory[index + i];
     }
 }
